52d6e7e1ec219e5ce9aeae295941e67b2907a1d7
gmorris <gillemorris@gmail.com>
2012-06-02 23:01:59 +1000

    messing with colors

--- src/Viewer.java
+++ src/Viewer.java
@@ -21,14 +21,19 @@ public class Viewer extends JFrame {
     private RoadMap m;
     private List<Car> c;
     private List<TrafficLight> l;
-    private boolean output = false;
+    private int init = 0;
 
     private static final int u = 10;
+    private static final int w = 3; // we require 2*w < u
     /* FIXME */ private static final int n = 40;
+
+    private static final Color backgroundColor = new Color(0x7e7e7e);
+    private static final Color roadColor = new Color(0x212323);
+    private static final int darkish = 0x3f;
 
     public Viewer() {
         super();
-        this.setSize(400, 400);
+        this.setSize(u*n, u*n);
         this.setVisible(true);
     }
 
@@ -36,20 +41,32 @@ public class Viewer extends JFrame {
         this.m = m;
         this.c = c;
         this.l = l;
-        output = true;
+        this.getContentPane().setBackground(new Color(0x7e7e7e));
+        init++;
         this.repaint(0);
     }
 
     public void paint(Graphics g) {
-        if (!output) {
+        if (init == 0) {
+            g.drawString("Still learning...", u*n/3, u*n/3);
+            return;
+        } else if (init == 1) {
+            g.drawString("Learned! Enjoy the show!", u*n/3, u*n/3);
+            g.setColor(backgroundColor);
+            g.fillRect(0, 0, u*n, u*n);
+            init = 2;
             return;
         }
+
         String apology = "should be less  flickery soon";
         // ^ to this end repaint(int, int, int, int) could be of
         // much help. i'm assuming java just takes fucking forever to
         // draw ~2000 pixels.
+        //
+        // edit: possibly just dwm as usual. it never did work with java
         g.drawString("Hello, world!", n*u/4, n*u/4);
         g.drawString(apology, n*u/32, n*u/4+2*u);
+        g.setColor(roadColor);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; ++j) {
                 Coords k = new Coords(i, j);
@@ -59,19 +76,23 @@ public class Viewer extends JFrame {
             }
         }
         
-        g.setColor(Color.blue);
         for (Car i : c) {
-            Coords j = i.getCoords();
-            int x = j.getX(), y = j.getY();
+            int x = i.getCoords().getX(), y = i.getCoords().getY();
+            g.setColor(
+                x == y ? new Color(darkish, darkish, darkish) :
+                x == 20 ? new Color(0, y < 20 ? 255*y/20 : darkish, 0) :
+                y == 20 ? new Color(0, 0, x < 20 ? 255*x/20 : darkish) :
+                    Color.magenta
+            );
             g.fillRect(u*x, u*y, u, u);
         }
 
         for (TrafficLight i : l) {
-            //int x = i.getCoords.getX(), y = i.getCoords().getY();
-            Coords j = i.getCoords();
-            int x = j.getX(), y = j.getY();
+            int x = i.getCoords().getX(), y = i.getCoords().getY();
+            g.setColor(i.horizontalGreen() ? Color.green : Color.red);
+            g.fillRect(u*x+w, u*y, u-2*w, w);
             g.setColor(!i.horizontalGreen() ? Color.green : Color.red);
-            g.fillRect(u*x, u*y, u, u);
+            g.fillRect(u*x, u*y+w, w, u-2*w);
         }
     }
 }
